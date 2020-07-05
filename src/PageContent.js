ad9eb6305f9657c711441847f6f72cb7149174a7
Hank Ke <59212751+hke-cheng@users.noreply.github.com>
2020-07-05 16:54:15 -0700

    useContext practice

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,27 +1,19 @@
-import React, {Component} from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "./contexts/ThemeContext";
 
-class PageContent extends Component {
+function PageContent(props) {
 
-  static contextType = ThemeContext;
-
-  constructor(props) {
-    super(props);
-    this.state = {  };
+  const { isDarkMode } = useContext(ThemeContext);
+  const styles = {
+    backgroundColor: isDarkMode ? "black" : "white",
+    height: "100vh",
+    width: "100vw"
   }
-  render() {
-    const { isDarkMode } = this.context;
-    const styles ={
-      backgroundColor: isDarkMode? "black" : "white",
-      height:"100vh",
-      width:"100vw"
-    }
-    return (
-      <div style={styles}>
-        {this.props.children}
-      </div>
-    );
-  }
+  return (
+    <div style={styles}>
+      {props.children}
+    </div>
+  );
 }
 
 export default PageContent;
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,25 +1,18 @@
-import React, { Component, createContext } from "react";
+import React, { useState, createContext } from "react";
+import useToggleState from "../hooks/useToggleState";
 
 export const ThemeContext = createContext();
 
-export class ThemeProvider extends Component {
+export function ThemeProvider(props) {
 
-  constructor(props) {
-    super(props);
-    this.state = { isDarkMode: true };
-    this.toggleTheme= this.toggleTheme.bind(this);
-  };
 
-  toggleTheme(){
-    this.setState({isDarkMode: !this.state.isDarkMode});
-  }
+  const [isDarkMode, toggleTheme] = useToggleState(true);
 
-  render() {
-    return (
-      <ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
-        {this.props.children}
-      </ThemeContext.Provider>
-    )
-  }
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  )
 
 }
